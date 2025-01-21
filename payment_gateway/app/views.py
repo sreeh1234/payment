2e9a43f28c308bc5d7dc3413cf765ed4b355dfdd
sreehari k <162795824+sreeh1234@users.noreply.github.com>
2025-01-21 16:37:58 +0530

    razor payment gateway

---
+++ payment_gateway/app/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse
+from django.conf import settings
+import razorpay
+import json
+from django.views.decorators.csrf import csrf_exempt
+# Create your views here.
+
+def home(request):
+    return render(request, "index.html")
+
+def order_payment(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        amount = request.POST.get("amount")
+        client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID,
+                                       settings.RAZORPAY_KEY_SECRET))
+        razorpay_order = client.order.create(
+            {"amount":int(amount) * 100, "currency":"INR", "payment_capture": "1"} 
+        )
+        order_id=razorpay_order['id']
+        order = Order.objects.create(
+            name=name, amount=amount, provider_order_id=order_id
+        )
+        order.save()
+        return render(
+            request,
+            "index.html",
+            {
+                "callback_url":"http://" + "127.0.0.1:8000" + "razorpay/callback",
+                "razorpay_key": settings.RAZORPAY_KEY_ID,
+                "order":order,
+            },
+        )
+    return render(request, "index.html")
+
+@csrf_exempt
+def callback(request):
+    def verify_signature(response_data):
+        client = rasorpay.Client(auth=(settings.RAZORPAY_KEY_ID,
+                                       settings.RAZORPAY_KEY_SECRET))
+        return client.utility.verify_payment_signature(response_data)
+    if "razorpay_signature" in request.POST:
+        payment_id = request.POST.get("razorpay_payment_id", "")
+        provider_order_id = request.POST.get("razorpay_payment_id", "")
+        signature_id = request.POST.get("razorpay_signature", "")
+        order = Order.objects.get(provider_order_id=provider_order_id)
+        order.payment_id = payment_id
+        order.signature_id = signature_id
+        order.save()
+        if not verify_signature(request.POST):
+            order_status = PaymentStatus.SUCCESS
+            order.save()
+            return render(request, "callback.html", context={"status": order.status})
+        else:
+            order_status = PaymentStatus.FAILURE
+            order.save()
+            return render(request, "callback.html", context={"status": order.status})
+    else:
+        payment_id = json.loads(request.POST.get("error[metadata]")).get("payment_id")    
+        provider_order_id = json.loads(request.POST.get("error[metadata]")).get("order_id")
+        order = Order.objects.get(provider_order_id=provider_order_id)
+        order.payment_id = payment_id
+        order.status = PaymentStatus.FAILURE
+        order.save()
+        return render(request, "callback.html", context={"status":order.status})
